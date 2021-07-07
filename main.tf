362a7ee7050dc913978f9b82e2a2ac777f79e92f
dale <dale@localhost.localdomain>
2021-07-07 21:57:07 +0800

    adjust security group parameter

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "example" {
     ami = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
     
-    vpc_security_group.instance.id = [aws_security_group.instance.id]
+    vpc_security_group_ids = [aws_security_group.instance.id]
 
     user_data = <<-EOF
                 #!/bin/bash
